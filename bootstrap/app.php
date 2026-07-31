eb864d9bafb7dc56883dd5f2765c7af1a7024556
m-elmohands <mohameddelmohands@gmail.com>
2026-07-31 22:29:47 +0300

    handle error exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +19,31 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function (ValidationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return apiResponse(
+                    $e->errors(),
+                    'In-valid Inputs',
+                    422
+                );
+            }
+        });
+
+        $exceptions->renderable(function (ModelNotFoundException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return apiResponse(
+                    message: 'Resource not found',
+                    status: 404
+                );
+            }
+        });
+
+        $exceptions->renderable(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return apiResponse(
+                    message: 'Unauthenticated',
+                    status: 401
+                );
+            }
+        });
     })->create();
